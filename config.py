60bbc7545928dc1b2a6c40b387931fcebe816db4
JohnnySn0w <33181572+JohnnySn0w@users.noreply.github.com>
2024-04-04 16:06:49 -0500

    Paralellize frame generation

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stderr, level=logging.FATAL)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
--- generate_movie.py
+++ generate_movie.py
@@ -1,58 +1,136 @@
-from PIL import Image, ImageSequence, ImageFile
-from multiprocessing import Pool, shared_memory
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-import pandas as pd
 import numpy as np
+import pandas as pd
+from PIL import Image, ImageSequence, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 from moviepy.editor import ImageSequenceClip
-import sys
-import math
+import time
+from multiprocessing import Pool, shared_memory
+
 from config import *
 
-'''Functions'''
-def create_frame(frame_number):
-    # Create a blank canvas
-    canvas = Image.new("RGBA", canvas_size, (255, 255, 255, 0))
-    blank = Image.new("RGBA", (banner_width, banner_height), (0, 0, 0, 0))  # for blank frames
+"""Functions"""
+
+
+def create_frame(
+    df,
+    dtype,
+    frame_mem_name,
+    frame_number,
+    canvas_size,
+    banner_width,
+    banner_height,
+):
+    logging.info(f"processing frame {frame_number} in {frame_mem_name}")
+    # Height and width are swapped in PIL, adjust for that when using canvas_size dimensions
+    shape = (canvas_size[1], canvas_size[0], 4)
+    frame_mem = shared_memory.SharedMemory(name=frame_mem_name, create=False)
+    frame_array = np.ndarray(shape=shape, dtype=dtype, buffer=frame_mem.buf)
+    frame_array.fill(0)
+
     for _, row in df.iterrows():
         image_path = row["func_result"]
         # Adjust x, y to scale up to the new grid
         x, y = int(row["x"]) * banner_width, int(row["y"]) * banner_height
 
-        with Image.open(f"./buttons/{image_path}") as img:
-            # Handle animated images
-            if "duration" in img.info:
-                try:
+        try:
+            with Image.open(f"./buttons/{image_path}") as img:
+                # Handle animated images
+                if "duration" in img.info:
                     frames = [frame.copy() for frame in ImageSequence.Iterator(img)]
                     img_frame = frames[frame_number % len(frames)]
-                except IndexError as e:
-                    logging.error(f"Tried to parse frames but got {e}")
-                    img_frame = blank
-            else:
-                img_frame = img
+                else:
+                    img_frame = img.convert("RGBA")
 
-            img_frame = img_frame.convert("RGBA")
+                # Catch conversion errors
+                if img_frame.mode != "RGBA":
+                    img_frame = img_frame.convert("RGBA")
 
-            # Paste the image frame onto the canvas
-            canvas.paste(img_frame, (x, y))
-
-    return np.array(canvas)
+                # Catch sizing errors
+                if (
+                    img_frame.size[0] > banner_width
+                    or img_frame.size[1] > banner_height
+                ):
+                    img_frame = img_frame.crop((0, 0, banner_width, banner_height))
+                # Paste the image frame onto the canvas
+                img_array = np.array(img_frame)
+                frame_array[y : y + banner_height, x : x + banner_width] = img_array
+        except IndexError as e:
+            # logging.error(f"Tried to parse frames but got {e}")
+            # canvas.paste(blank, (x, y))
+            pass
+    # frame_array = np.array(canvas)
+    frame_mem.close()
 
 
 def gen_movie():
     # Determine number of frames for the video (based on the animated images)
-    num_frames = 30  # Adjust based on your needs
+    num_frames = 30  # for testing. Later, frames should be based on the maximum animated gif used's frames
 
-    # Create the frames for the video
-    frames = [create_frame(f) for f in range(num_frames)]
+    # Load CSV
+    df = pd.read_csv("pixel_results.csv")
+    orig_img = Image.open("downscaled.png")
+
+    # Dimensions of the replacement images
+    banner_width, banner_height = 88, 31
+
+    # Calculate canvas size based on the original image dimensions
+    canvas_size = (orig_img.width * banner_width, orig_img.height * banner_height)
+    num_channels = 4
+    dtype = np.uint8
+    size = int(np.prod(canvas_size) * num_channels * np.dtype(dtype).itemsize)
+
+    shape = (canvas_size[1], canvas_size[0], num_channels)
+
+    # Create bundle of shared_mem objs, one per frame
+    frame_mem_objs = [
+        shared_memory.SharedMemory(create=True, size=size) for _ in range(num_frames)
+    ]
+    frame_names = [frame.name for frame in frame_mem_objs]
+
+    # Create pool and exec
+    args = [
+        (
+            df,
+            dtype,
+            frame_names[frame_number],
+            frame_number,
+            canvas_size,
+            banner_width,
+            banner_height,
+        )
+        for frame_number in range(num_frames)
+    ]
+    logging.info("Executing process pool")
+    with Pool() as pool:
+        pool.starmap(create_frame, args)
+
+    # Now that the sh_mem objs are populated w/ frame data, collate them into a list of np arrays
+    processed_frames = [
+        np.ndarray(
+            shape=shape,
+            dtype=dtype,
+            buffer=frame.buf,
+        )
+        for frame in frame_mem_objs
+    ]
 
     # Create the video
-    clip = ImageSequenceClip(frames, fps=10)  # Adjust FPS as needed
+    logging.info("processing video")
+    clip = ImageSequenceClip(processed_frames, fps=10)  # Adjust FPS as needed
     clip.write_videofile(
         "output_video.mp4", codec="libx264", audio_codec="aac"
     )  # https://stackoverflow.com/a/70826414
 
+    # cleanup
+    logging.info("Cleaning up")
+    for frame in frame_mem_objs:
+        frame.close()
+        frame.unlink()
+    logging.info("Cleanup complete")
+
+
 if __name__ == "__main__":
-    dtype = np.uint8
-    shared_mem_obj = shared_memory.SharedMemory(name="memchunk", create=True, size=np.prod(orig_img.size)*dtype().itemsize)
-    shared_array = np.ndarray(orig_img.size, dtype=dtype, buffer=shared_mem_obj.buf)
+    t0 = time.time()
     gen_movie()
+    t1 = time.time()
+    logging.info(f"total time is {t1-t0}")
